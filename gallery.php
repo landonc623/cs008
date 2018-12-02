64882611880464c0b037bfc0158153d5f78a30ce
laurenpaicopolis <44977115+laurenpaicopolis@users.noreply.github.com>
2018-12-02 16:22:06 -0500

    Update gallery.php
    Images I used, feel free to change them!

--- gallery.php
+++ gallery.php
@@ -1,15 +1,15 @@
 <?php include '../top.php' ?>
 <main>
     <section class="gallery-images">
-        <figure></figure>
-        <figure></figure>
-        <figure></figure>
-        <figure></figure>
-        <figure></figure>
-        <figure></figure>
-        <figure></figure>
-        <figure></figure>
-        <figure></figure>
+        <figure> alt='Landscape of Mountains' src="images/landscape.jpeg"</figure>
+        <figure> alt='Sunset at UVM' src="images/sunset.jpeg"</figure>
+        <figure> alt='Mountains in Iceland' src="images/mountain.jpeg"</figure>
+        <figure> alt='Glass Window' src="images/glass.jpeg"</figure>
+        <figure> alt='Blue Lagoon in Iceland' src="images/bluelagoon.jpeg"</figure>
+        <figure> alt='More mountains in Iceland' src="images/mountains2.jpeg"</figure>
+        <figure> alt='A rocky river' src="images/waterfall.jpeg"</figure>
+        <figure> alt='Buildings in Iceland' src="images/buildings.jpeg"</figure>
+        <figure> alt='Large Waterfall in Iceland' src="images/bigwaterfall.jpeg"</figure>
     </section>
 </main>
 
