dc14a9e37c5f4dc6e06fdfcde1aee8eb30521b52
laurenpaicopolis <44977115+laurenpaicopolis@users.noreply.github.com>
2018-12-02 16:24:58 -0500

    Update gallery.php

--- gallery.php
+++ gallery.php
@@ -1,15 +1,33 @@
 <?php include '../top.php' ?>
 <main>
     <section class="gallery-images">
-        <figure> alt='Landscape of Mountains' src="images/landscape.jpeg"</figure>
-        <figure> alt='Sunset at UVM' src="images/sunset.jpeg"</figure>
-        <figure> alt='Mountains in Iceland' src="images/mountain.jpeg"</figure>
-        <figure> alt='Glass Window' src="images/glass.jpeg"</figure>
-        <figure> alt='Blue Lagoon in Iceland' src="images/bluelagoon.jpeg"</figure>
-        <figure> alt='More mountains in Iceland' src="images/mountains2.jpeg"</figure>
-        <figure> alt='A rocky river' src="images/waterfall.jpeg"</figure>
-        <figure> alt='Buildings in Iceland' src="images/buildings.jpeg"</figure>
-        <figure> alt='Large Waterfall in Iceland' src="images/bigwaterfall.jpeg"</figure>
+        <figure> 
+            <img alt='Landscape of Mountains' src="images/landscape.jpeg">
+        </figure>
+        <figure>
+            <img alt='Sunset at UVM' src="images/sunset.jpeg">
+        </figure>
+        <figure> 
+            <img alt='Mountains in Iceland' src="images/mountain.jpeg">
+        </figure>
+        <figure>
+            <img alt='Glass Window' src="images/glass.jpeg">
+        </figure>
+        <figure>
+            <img alt='Blue Lagoon in Iceland' src="images/bluelagoon.jpeg">
+        </figure>
+        <figure> 
+            <img alt='More mountains in Iceland' src="images/mountains2.jpeg">
+        </figure>
+        <figure> 
+            <img alt='A rocky river' src="images/waterfall.jpeg">
+        </figure>
+        <figure> 
+            <img alt='Buildings in Iceland' src="images/buildings.jpeg">
+        </figure>
+        <figure> 
+            <img alt='Large Waterfall in Iceland' src="images/bigwaterfall.jpeg">
+        </figure>
     </section>
 </main>
 
